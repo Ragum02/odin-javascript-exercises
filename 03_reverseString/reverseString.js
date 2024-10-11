3d0e52a302da929b9e7458d1a90f9067b89e0ab1
Ragum02 <benjamin.souiket@gmail.com>
2024-10-11 15:21:22 +0200

    exercice 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+const tempArray = string.split("").reverse()
+string = tempArray.join("");
+return string;
 };
 
 // Do not edit below this line
