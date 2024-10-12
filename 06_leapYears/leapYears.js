acf809539924ff14a2192d2df643b5ed3c07d101
Ragum02 <benjamin.souiket@gmail.com>
2024-10-12 09:11:38 +0200

    ex 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year,rem) {
+const isLeap = year % 4 === 0
+const isLeapToo = year % 400 === 0;
+const isCentury = year % 100 === 0;
 
+if(isLeap && isLeapToo || !isCentury) {
+    return true;
+} else {
+    return false;
+}
 };
 
 // Do not edit below this line
