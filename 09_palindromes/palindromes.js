cdb8482d2a1f5f6524088f4ffd82fe9f9aebce5c
Ragum02 <benjamin.souiket@gmail.com>
2024-10-17 14:59:12 +0200

    palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+const alphanum = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+const verif = str.toLowerCase().split("").filter(character => alphanum.includes(character)).join("");
+
+const reversed = verif.split("").reverse().join("")
+return reversed === verif;
 };
 
 // Do not edit below this line
