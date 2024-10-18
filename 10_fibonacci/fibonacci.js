ce4d3deb0c9511402f341a9d5282e26df56676b9
Ragum02 <benjamin.souiket@gmail.com>
2024-10-18 09:57:19 +0200

    fibo exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let count = 0;
+
+
+    if (typeof a !== 'number') {
+        count = parseInt(a)
+    } else {
+        count = a
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+
+const fibo = [0,1];
+for(let i = 2; i <= count; i++){
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+}
+
+
+return fibo[count];
+
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
